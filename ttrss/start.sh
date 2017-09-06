b639d1197c5ff03a30e38ed4836afd7c322bb08a
nouchka <github@katagena.com>
2017-09-06 19:10:38 +0000

    typo

--- ttrss/start.sh
+++ ttrss/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-DAY=`date +%Y%m`
-echo "Export OPML ($DAY)"
+MONTH=`date +%Y%m`
+echo "Export OPML ($MONTH)"
 
 wget -q -O- --output-document=/data/opml.xml "http://ttrss/backend.php?op=opml&method=export&filename=ttrss.opml&settings=1"
 
-cp -f /data/opml.xml /data/opml-$DAY.xml
+cp -f /data/opml.xml /data/opml-$MONTH.xml
