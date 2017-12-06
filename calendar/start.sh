611bfc574582ff9b689559b4b48c09945840ee79
nouchka <github@katagena.com>
2017-12-06 16:52:21 +0000

    name

--- calendar/start.sh
+++ calendar/start.sh
@@ -3,9 +3,9 @@
 mkdir -p /data/
 cd /data/
 
-for URL in $URLS
+for URL in $CALENDAR_URLS
 do
-	wget -q -r -O ${URL##*/} --auth-no-challenge --user=$USER --password=$PASSWORD $URL
+	wget -q -r -O ${URL##*/} --auth-no-challenge --user=$CALENDAR_USER --password=$CALENDAR_PASSWORD $URL
 	if [ $? -ne 0 ]; then
 		>&2 echo "Fail to download $URL"
 	fi
