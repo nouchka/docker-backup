0ebc681f486e4dce5f2805272a2fbb8459f124c1
nouchka <dev@zen-spirit.net>
2019-08-05 13:43:07 +0000

    backup pgsql

--- elephantsql/start.sh
+++ elephantsql/start.sh
@@ -5,6 +5,8 @@ mkdir -p /data/
 MONTH=`date +%Y%m`
 echo "Export Elephantsql pgsql data ($MONTH)"
 
-wget -qO- --user= --password=$DB_APIKEY https://api.elephantsql.com/api/backup?db=$DB_USER|jq -r '.[0]| .url'|xargs wget -q --output-document=$DB_USER.$MONTH.sql.lzo
+wget -qO- --user= --password=$DB_APIKEY https://api.elephantsql.com/api/backup?db=$DB_USER|jq -r '.[0]| .url'|xargs wget -q --output-document=$DB_NAME.$MONTH.sql.lzo
+
+cp $DB_NAME.$MONTH.sql.lzo $DB_USER.sql.lzo
 
 /backup.sh
