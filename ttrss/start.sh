67580b35b01ebdd0bba8239615369ae9b0864ce6
nouchka <dev@zen-spirit.net>
2019-08-06 20:08:50 +0000

    tt-rss opml reference on heroku

--- ttrss/start.sh
+++ ttrss/start.sh
@@ -3,7 +3,7 @@
 MONTH=`date +%Y%m`
 echo "Export OPML ($MONTH)"
 
-wget -q -O- --output-document=/data/opml.xml "http://ttrss/backend.php?op=opml&method=export&filename=ttrss.opml&settings=1"
+wget -q -O- --output-document=/data/opml.xml "$TTRSS_OPML_URL"
 
 cp -f /data/opml.xml /data/opml-$MONTH.xml
 
